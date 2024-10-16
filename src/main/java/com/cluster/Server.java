922118a12341e9ec2b423725ade1b3061f6d51bb
vinicius-dantasso <vinicius.dantas777@outlook.com>
2024-10-16 17:56:12 -0300

    update: servidor centralizado recebendo todas as infos e servidores agora enviam as infos individualmente

--- src/main/java/com/cluster/Server.java
+++ src/main/java/com/cluster/Server.java
@@ -1,18 +1,30 @@
 package com.cluster;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.messages.Metrics;
+import com.messages.ServerState;
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.Random;
+import java.time.Instant;
 
 public class Server {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
 
-    private static final String DBSERVER_CONN = "dbServerConn";
-    private static final String WEBSERVER_CONN = "webServerConn";
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-    private static final int serverNumber = 3;
+    private static final String LOGSERVER_CONN = "logServerConn";
+
+    private static final int serverNumber = 1;
     private static final String serverKey = "server" + serverNumber;
 
     public static void main(String[] args) throws Exception {
@@ -21,46 +33,95 @@ public class Server {
 
         System.out.println("Server " + serverNumber + " iniciado.");
 
-        receiveData("localhost", DBSERVER_CONN);
-        receiveData("localhost", WEBSERVER_CONN);
+        receiveData("localhost", "database");
+        receiveData("localhost", "web server");
 
     }
 
-    private static void receiveData(String ip, String CONNECTION_NAME) throws Exception {
+    private static void receiveData(String ip, String serviceName) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(ip);
 
         Connection conn = factory.newConnection();
         final Channel channel = conn.createChannel();
 
-        channel.exchangeDeclare(CONNECTION_NAME, "direct");
+        channel.exchangeDeclare(LOGSERVER_CONN, BuiltinExchangeType.TOPIC);
 
-        // criar uma fila temporária p vincular à exchange com a chave dos servers
-        String temporaryQueue = channel.queueDeclare().getQueue();
-        channel.queueBind(temporaryQueue, CONNECTION_NAME, serverKey);
-
-        System.out.println(" [*] Esperando mensagens...");
-
-        DeliverCallback callbackEntrega = (tagConsumidor, environmentRead) -> {
-            // essa aq de baixo é p transformar em de json p objeto e ler os valores individualmente mais fácil
-            //EnvironmentRead lastRead = objectMapper.readValue(environmentRead.getBody(), EnvironmentRead.class);
-            String jsonMessage = new String(environmentRead.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" Recebida <-- " + jsonMessage);
-
+        scheduler.scheduleAtFixedRate(() -> {
             try {
-                // fazer algo
-            } finally {
-                System.out.println("------------------------------------------");
-                // faz o reconhecimento das mensagens da fila vinculada
-                channel.basicAck(environmentRead.getEnvelope().getDeliveryTag(), false); // tira da fila do RabbitMQ
+                ServerState serverState = getServerState(serverKey, serviceName);
+                String jsonMessage = objectWriter.writeValueAsString(serverState);
+                String routingKey = setRoutingKey(jsonMessage);
+
+                channel.basicPublish(
+                    LOGSERVER_CONN,
+                    routingKey,
+                    null,
+                    jsonMessage.getBytes(StandardCharsets.UTF_8)
+                );
+                System.out.println("Enviando --> " + jsonMessage);
             }
-
-        };
-
-        // consome as mensagens da fila vinculada
-        channel.basicConsume(temporaryQueue, false, callbackEntrega, consumerTag -> { });
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 3, TimeUnit.SECONDS);
     }
 
+    private static String setRoutingKey(String message) {
+        if(message.contains("server1") && message.contains("database")) {
+            return "server1/database";
+        }
+        else if(message.contains("server2") && message.contains("database")) {
+            return "server2/database";
+        }
+        else if(message.contains("server3") && message.contains("database")) {
+            return "server3/database";
+        }
+        else if(message.contains("server1") && message.contains("web server")) {
+            return "server1/web_server";
+        }
+        else if(message.contains("server2") && message.contains("web server")) {
+            return "server2/web_server";
+        }
+        else if(message.contains("server3") && message.contains("web server")) {
+            return "server3/web_server";
+        }
 
+        return null;
+    }
+
+    private static ServerState getServerState(String destinyServer, String service) {
+
+        ServerState state = new ServerState();
+
+        state.setTimestamp(Instant.now().toString());
+        state.setService(service);
+        state.setServer(destinyServer);
+
+        // definir melhor como pegar essas métricas
+        Random rand = new Random();
+        Metrics metrics = new Metrics();
+        metrics.setCpu_usage(rand.nextInt(100));
+        metrics.setMemory_usage(rand.nextInt(100));
+        metrics.setResponse_time(rand.nextInt(1000));
+
+        state.setMetrics(metrics);
+
+        if(metrics.getCpu_usage() >= 94 ||
+        metrics.getMemory_usage() >= 90 ||
+        metrics.getResponse_time() >= 900) {
+            state.setStatus("vermelho");
+        }
+        else if((metrics.getCpu_usage() >= 75 && metrics.getCpu_usage() < 94) ||
+        (metrics.getMemory_usage() >= 75 && metrics.getMemory_usage() < 90) ||
+        metrics.getResponse_time() >= 600 && metrics.getResponse_time() < 900) {
+            state.setStatus("amarelo");
+        }
+        else {
+            state.setStatus("azul");
+        }
+
+        return state;
+    }
 
 }
--- src/main/java/com/monitoring/CentralizedServer.java
+++ src/main/java/com/monitoring/CentralizedServer.java
@@ -1,11 +1,34 @@
 package com.monitoring;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
 public class CentralizedServer {
 
-    public static void main(String[] args) {
+    private static final String LOGSERVER_CONN = "logServerConn";
 
-        System.out.println("Centralized Monitoring Server started");
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
+        channel.exchangeDeclare(LOGSERVER_CONN, BuiltinExchangeType.TOPIC);
+        String queueName = channel.queueDeclare().getQueue();
+
+        String routingKey = "#";
+        channel.queueBind(queueName, LOGSERVER_CONN, routingKey);
+
+        System.out.println("[*] Aguardando mensagens...");
+
+        DeliverCallback callback = (tagConsumer, entrega) -> {
+            String message = new String(entrega.getBody(), "UTF-8");
+            System.out.println("Recebida <-- " + entrega.getEnvelope().getRoutingKey() + ": " + message);
+        };
+        channel.basicConsume(queueName, true, callback, tagConsumer -> {});
     }
 
 }
